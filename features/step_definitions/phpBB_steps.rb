b24471666a4ead0f2829747c4e047d94e3edb8cb
csmalley <csmalley@xpanxion.com>
2013-05-07 13:51:39 -0500

    First Commit

---
+++ features/step_definitions/phpBB_steps.rb
@@ -0,0 +1,124 @@
+require 'pry'
+require 'watir'
+
+Given(/^I am a not a registered user on the site (.*?)$/) do |link|
+  #pending
+
+  @site = Watir::Browser.new :firefox
+  @site.goto(link)
+end
+
+When(/^I register as a new user$/) do
+
+  @site.link(:text =>'Register').click
+  @site.input(:id => 'agreed').click
+  @rand = Random.rand(10000)
+  @site.text_field(:id => 'username').value = ('test'+ @rand.to_s + '@junk.com')
+  @site.text_field(:id => 'email').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'email_confirm').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'new_password').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'password_confirm').value = @site.text_field(:id => 'username').value
+  @site.input(:id => 'submit').click
+
+end
+
+Then(/^I should see the Thank You page$/) do
+
+  @site.html.include? ('#Thank you for registering')
+  @site.close
+end
+
+
+Given(/^I am a returning user to the site (.*?)$/) do |link|
+  #pending # express the regexp above with the code you wish you had
+  @site = Watir::Browser.new :firefox
+  @site.goto(link)
+  @site.link(:text =>'Register').click
+  @site.input(:id => 'agreed').click
+  @rand = Random.rand(10000)
+  @site.text_field(:id => 'username').value = ('test'+ @rand.to_s + '@junk.com')
+  @username = @site.text_field(:id => 'username').value
+
+  puts 'Username = #{@username}'
+
+  @site.text_field(:id => 'email').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'email_confirm').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'new_password').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'password_confirm').value = @site.text_field(:id => 'username').value
+  @site.input(:id => 'submit').click
+
+  @site.link(:title =>'Login').click
+
+  @site.text_field(:id => 'username').set (@username)
+  @site.text_field(:id => 'password').set (@username)
+  @site.input(:name =>'login').click
+
+
+end
+
+When(/^I enter the User Control Panel$/) do
+  @site.link(:title =>'User Control Panel').click
+  @site.span(:text => 'Profile').click
+
+end
+
+Then(/^I should be able to save changes to my profile$/) do
+  #pending # express the regexp above with the code you wish you had
+
+  @site.text_field(:id => 'website').set ('http://www.b-body.org')
+  @site.text_field(:id => 'location').set ('Husker Nation!')
+  @site.select_list(:id => 'bday_day').select ('5')
+  @site.select_list(:id => 'bday_month').select ('5')
+  @site.select_list(:id => 'bday_year').select ('1977')
+  @site.input(:name => 'submit').click
+  @site.link(:text => 'Board index').click
+
+  @site.close
+
+
+end
+
+
+Given(/^I am a returning registered user to the site(.*?)$/) do |link|
+  @site = Watir::Browser.new :firefox
+  @site.goto(link)
+  @site.link(:text =>'Register').click
+  @site.input(:id => 'agreed').click
+  @rand = Random.rand(10000)
+  @site.text_field(:id => 'username').value = ('test'+ @rand.to_s + '@junk.com')
+  @username = @site.text_field(:id => 'username').value
+
+  puts 'Username = #{@username}'
+
+  @site.text_field(:id => 'email').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'email_confirm').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'new_password').value = @site.text_field(:id => 'username').value
+  @site.text_field(:id => 'password_confirm').value = @site.text_field(:id => 'username').value
+  @site.input(:id => 'submit').click
+  #sleep 3
+  @site.link(:title =>'Login').click
+
+  @site.text_field(:id => 'username').set (@username)
+  #@site.send_keys :tab
+  @site.text_field(:id => 'password').set (@username)
+  @site.input(:name =>'login').click
+
+
+end
+
+When(/^I enter the forum (.*?)$/) do |forum|
+  #pending # express the regexp above with the code you wish you had
+  @site.link(:text => forum).wait_until_present
+  @site.link(:text => forum).click
+
+end
+
+Then(/^I should be able to create a new thread$/) do
+
+  @site.link(:text => 'Post a new topic').click
+  @site.text_field(:id=>'subject').set  ('testing 123')
+  @site.text_field(:name => 'message').set  ('What do I actually get in this field')
+  @site.input(:name => 'post').click
+  @site.close
+
+  end
---
+++ features/user_profile.feature
@@ -0,0 +1,32 @@
+Feature:
+
+  As a phpBB website owner
+  I want users to be able to register, edit their settings and post on my site.
+
+
+@RegisteringNewUser
+Scenario: Registering A New User
+  Given I am a not a registered user on the site http://xpan.b-body.org
+  When I register as a new user
+  Then I should see the Thank You page
+
+#@ControlPanelTest
+Scenario: Editing User Settings
+  Given I am a returning user to the site http://xpan.b-body.org
+  When I enter the User Control Panel
+  Then I should be able to save changes to my profile
+
+
+@PostingToThread
+Scenario: Posting To A Thread
+  Given I am a returning registered user to the site http://xpan.b-body.org
+  When I enter the forum Forum Test 1
+  Then I should be able to create a new thread
+
+
+
+#Scenario: Post To An Existing Thread
+#  Given I am a returning registered user to the site
+#  When I enter a forum with an existing thread
+#  Then I should be able to post to the existing thread
+
